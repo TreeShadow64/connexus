c3a3587254e1c891d28aecdf7b9c024e79582af9
TreeShadow64 <dario.ryzza@gmail.com>
2026-08-13 00:34:13 +0200

    Bump versione a 0.26

--- android-client/app/build.gradle.kts
+++ android-client/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.hubpc.client"
         minSdk = 24
         targetSdk = 34
-        versionCode = 25
-        versionName = "0.25"
+        versionCode = 26
+        versionName = "0.26"
     }
 
     buildTypes {
